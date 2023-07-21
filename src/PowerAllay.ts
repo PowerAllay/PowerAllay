cd809a3d4e8fe975a745370e4e413422735ce645
taylordevs <taylordevs47@gmail.com>
2023-07-21 20:19:48 +0700

    More info will log when run server

--- src/PowerAllay.ts
+++ src/PowerAllay.ts
@@ -1,7 +1,12 @@
 import { Config, types } from "./utils/Config";
 import * as path from "path";
 import { BaseLogger } from "./utils/BaseLogger";
-import {Language, languages} from "./languages/Language";
+import { Language } from "./languages/Language";
+
+export const VersionInfo = {
+    name: "PowerAllay",
+    version: "1.0.0"
+};
 
 export class PowerAllay {
     private properties: Config;
@@ -9,13 +14,24 @@ export class PowerAllay {
     private readonly dataPath: string;
     private readonly baselogger: BaseLogger;
 
+    /**
+     * @constructor
+     */
     constructor() {
         this.dataPath = path.join(__dirname, '..');
         this.baselogger = new BaseLogger(this);
         this.initProperties();
         this.initLanguage();
+        this.getLogger().info(
+            this.getLanguage().translate("running-info", VersionInfo.name, VersionInfo.version)
+        );
     }
 
+    /**
+     * Initialize server properties
+     *
+     * @private
+     */
     private initProperties() {
         this.getLogger().info('Loading server properties...');
         this.properties = new Config(`${this.getDataPath()}/server.json`, types.json, {
@@ -28,6 +44,11 @@ export class PowerAllay {
         })
     }
 
+    /**
+     * Initialize server language
+     *
+     * @private
+     */
     private initLanguage() {
         this.language = new Language(this);
         this.getLogger().info(
@@ -35,22 +56,37 @@ export class PowerAllay {
         );
     }
 
+    /**
+     * Get server data path
+     */
     getDataPath() {
         return this.dataPath;
     }
 
+    /**
+     * Get server properties
+     */
     getProperties() {
         return this.properties;
     }
 
+    /**
+     * Get server logger
+     */
     getLogger() {
         return this.baselogger;
     }
 
+    /**
+     * Get server language
+     */
     getLanguage() {
         return this.language;
     }
 
+    /**
+     * Start server
+     */
     stop() {
         this.getLogger().info('Stopping server...');
         process.exit(0);
